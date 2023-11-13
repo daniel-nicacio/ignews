7aec506fee9f5e107881ec2df1aceeb9e755d1ac
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2023-11-12 23:31:36 -0300

    feat: adding faunaDB configuration and saving user to database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +14,37 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(query.Collection("users"), { data: { email } }),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
